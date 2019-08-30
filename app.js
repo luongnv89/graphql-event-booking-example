cfafb867b9ea4b0c210d772e1382002300c1af3a
Luong NGUYEN <luongnv89@gmail.com>
2019-08-30 17:12:18 +0200

    Lesson 13: Hitting the API

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const mongoose = require("mongoose");
 
-const app = express();
-
 const mySchemas = require("./graphql/schema");
 const myResolvers = require("./graphql/resolvers");
-const isAuth = require('./middlewares/is-auth');
+const isAuth = require("./middlewares/is-auth");
+
+const app = express();
+
 app.use(bodyParser.json());
+
+// Middlewares
+
+app.use((req, res, next) => {
+  console.log("Receive a request: ", req.method);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Autorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  console.log("Going to check the authentication ", req.method);
+  next();
+});
 
 app.use(isAuth);
 
