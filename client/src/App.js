2f29fa71e7ce3f8ab06ce715bc215df46229462f
Luong NGUYEN <luongnv89@gmail.com>
2019-09-09 16:03:36 +0200

    use custom React hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
 
@@ -7,65 +7,48 @@ import BookingPage from "./pages/Booking";
 import EventPage from "./pages/Event";
 import Navigation from "./components/Navigation";
 
-import AuthContext from './context/auth-context';
+import AuthContext from "./context/auth-context";
 
-class App extends Component {
-  state = {
-    token: null,
-    userId: null
-  }
+// import { useLocalStorage } from './hooks/useLocalStorage';
+import { useLocalStorage } from 'react-hooks-crocode';
 
-  componentDidMount = () => {
-    const token = sessionStorage.getItem('TOKEN');
-    const userId = sessionStorage.getItem('USERID');
-    if (token && userId) {
-      this.setState({
-        token,
-        userId
-      });
-    }
-  }
+const App = props => {
 
-  login = (token, userId, tokenExpiration) => {
-    sessionStorage.setItem('TOKEN', token);
-    sessionStorage.setItem('USERID', userId);
-    this.setState({
-      token,
-      userId
-    });
-  }
+  const [token, setToken ] = useLocalStorage('TOKEN');
+  const [userId, setUserId] = useLocalStorage('USERID');
 
-  logout = () => {
-    sessionStorage.removeItem('TOKEN');
-    sessionStorage.removeItem('USERID');
-    this.setState({
-      token: null,
-      userId: null
-    })
-  }
+  const login = (token, userId, tokenExpiration) => {
+    setToken(token);
+    setUserId(userId);
+  };
 
-  render() {
-    return (
+  const logout = () => {
+    setToken(null);
+    setUserId(null);
+  };
+
+  return (
     <BrowserRouter>
-      <AuthContext.Provider value={{token: this.state.token, userId: this.state.userId, login: this.login, logout: this.logout}}>
+      <AuthContext.Provider
+        value={{ token: token, userId: userId, login: login, logout: logout }}
+      >
         <Navigation />
         <main className="main-content">
           <Switch>
-            {this.state.token && <Redirect from="/" to="events" exact />}
-            {this.state.token && <Redirect from="/auth" to="events" exact />}
+            {token && <Redirect from="/" to="events" exact />}
+            {token && <Redirect from="/auth" to="events" exact />}
 
-            {!this.state.token && <Route path="/auth" component={AuthPage} />}
-            {this.state.token && <Route path="/bookings" component={BookingPage} />}
+            {!token && <Route path="/auth" component={AuthPage} />}
+            {token && <Route path="/bookings" component={BookingPage} />}
 
             <Route path="/events" component={EventPage} />
 
-            {!this.state.token && <Redirect to="auth" exact />}
+            {!token && <Redirect to="auth" exact />}
           </Switch>
         </main>
       </AuthContext.Provider>
     </BrowserRouter>
   );
-  }
-}
+};
 
 export default App;
